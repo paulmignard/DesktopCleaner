98b7acdcebbc93f12a11954e7393e6b57d84214f
paulmignard <paul@mignard.co>
2022-07-16 11:20:29 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DesktopCleaner
 This app cleans up all of the ideas and screenshots I save to my desktop and places them in an idea folder in iCloud.
+
+I'm new to ruby.
---
+++ desktop-cleaner.rb
@@ -0,0 +1,39 @@
+#Alright, ruby time!
+require 'securerandom'
+
+# Config 
+$desktop_directory = "/users/paulmignard/Desktop/"
+$ideas_directory = "/users/paulmignard/Documents/_ideas/"
+
+# Counters
+$file_count = 0
+
+puts("Starting cleanup, let's do this! 💪🏻")
+
+# Well, let's find some files!
+Dir[$desktop_directory+"*"].each do |file_name|
+    
+    # Is this a directory? If so, NEXT
+    next if File.directory? file_name 
+    
+    # Is this an iphone or mac screenshot?
+    next if not file_name.include? "PNG image" and not file_name.include? "Screen Shot"
+
+    # Great, if we're here we have a file that's *probably* an idea!
+    $file_count += 1
+    file_create_time = File.birthtime(file_name)
+    new_file_name = "i-" + file_create_time.year.to_s + "-" + file_create_time.month.to_s + "-" + file_create_time.day.to_s + "-" + SecureRandom.uuid
+    puts new_file_name
+
+    # Alright, let's move it!
+    # NOTE: This won't work across partitions on nix systems? 🤔
+    File.rename file_name, $ideas_directory + new_file_name
+
+  end
+
+  # All Done!
+  puts(" ")
+  puts("All Done! 🥳")
+  puts("Files Changed:          " + $file_count.to_s)
+  puts(" ")
+
